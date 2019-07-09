29af85077deb4004cc84514f9b7d4ad36e1f9585
birkin <birkin.diana@gmail.com>
2019-07-09 12:43:51 -0400

    implements first-item check of Yoken bib.

--- checker.py
+++ checker.py
@@ -23,20 +23,84 @@ def run_checks():
     """ Manages functional-checks. """
     try:
         check_A()
+        yoken = YokenCheck()
+        yoken.run_check()
     except Exception:
         log.exception( 'exception; traceback...' )
         # raise
 
 
+class YokenCheck:
+
+    def __init__(self):
+        pass
+
+
+    def run_check(self):
+        """ Tests Hay `Archives/Manuscripts` `Mel B. Yoken collection` requirement. """
+
+        browser = self.load_page()
+
+        ## format
+        format_element = browser.find_elements_by_class_name( 'blacklight-format' )[1]  # [0] is the word 'Format'
+        assert format_element.text == 'Archives/Manuscripts', f'format_element.text, ```{format_element.text}```'
+
+        ## first item info
+        first_item = browser.find_elements_by_class_name( 'bib_item' )[0]
+        ( location, call_number, status ) = self.get_first_item_info( first_item )
+        for class_type in [ 'scan', 'jcb_url', 'hay_aeon_url', 'ezb_volume_url' ]:
+            empty_link_element = first_item.find_element_by_class_name( class_type )
+            assert empty_link_element.text == '', f'empty_link_element.text, ```{empty_link_element.text}```'
+
+        ## first item test -- link should NOT show
+        easyrequest_hay_url = first_item.find_element_by_class_name( 'annexhay_easyrequest_url' )
+        assert easyrequest_hay_url.text.strip() == '', f'easyrequest_hay_url.text, ```{easyrequest_hay_url.text}```'
+
+        log.info( f'Result: All good.' )  # won't get here unless all asserts pass
+
+        ## end def run_check()
+
+    def load_page( self ):
+        """ Hits url; returns browser object.
+            Called by run_check() """
+        aim = """\n-------\nGoal: Ensure a format of `Archives/Manuscripts` with a location of `HAY MANUSCRIPTS`
+        shows the easyrequest_hay request url -- _if_ the status is `AVAILABLE`. """
+        log.info( aim )
+        bib = 'b3589814'
+        url = f'{settings.ROOT_PAGE_URL}/{bib}'
+        log.info( f'hitting url, ```{url}```' )
+        #
+        browser.get( url )
+        return browser
+
+    def get_first_item_info( self, first_item ):
+        """ Parses item.
+            Called by run_check() """
+        # print( f'first_item.text, ```{first_item.text}```' )
+        location = first_item.find_element_by_class_name( 'location' )
+        assert location.text == 'HAY MANUSCRIPTS', f'location.text, ```{location.text}```'
+        #
+        call_number = first_item.find_element_by_class_name( 'callnumber' )
+        assert call_number.text == 'Oversize Box 1XX', f'call_number.text, ```{call_number.text}```'
+        #
+        status = first_item.find_element_by_class_name( 'status' )
+        assert status.text == '--', f'status.text, ```{status.text}```'
+        return ( location, call_number, status )
+
+    ## end class YokenCheck
+
+
 def check_A():
-    """ Tests Hay `Archives/Manuscripts` requirement. """
+    """ Tests Hay `Archives/Manuscripts` `David Beckwith papers` requirement. """
 
     ## load page
     aim = """\n-------\nGoal: Ensure a format of `Archives/Manuscripts` with a location of `ANNEX HAY` shows the easyrequest_hay request url. """
     log.info( aim )
-    log.info( f'hitting url, ```{settings.URL_A}```' )
+    bib = 'b5706110'
+    url = f'{settings.ROOT_PAGE_URL}/{bib}'
+    log.info( f'hitting url, ```{url}```' )
     #
-    browser.get( settings.URL_A )
+    browser.get( url )
 
     ## format
     format_element = browser.find_elements_by_class_name( 'blacklight-format' )[1]  # [0] is the word 'Format'
