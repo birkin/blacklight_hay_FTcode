4fae31171ce2cf63ae6e2e589af5df925b98de1b
birkin <birkin.diana@gmail.com>
2019-07-10 14:57:08 -0400

    moves wait to env-setting.

--- settings.py
+++ settings.py
@@ -2,3 +2,5 @@ import os
 
 ROOT_PAGE_URL = os.environ['BLK_HAY__ROOT_PAGE_URL']
 PRODUCTION_ROOT_PAGE_URL = 'https://search.library.brown.edu/catalog'
+
+BROWSER_WAIT_SECONDS = int( os.environ['BLK_HAY__BROWSER_WAIT'] )
