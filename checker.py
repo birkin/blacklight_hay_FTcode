d6b39c85601b5f2c8c510a174d57b75a94a4c8ac
birkin <birkin.diana@gmail.com>
2019-07-22 10:59:28 -0400

    adds caller for hay-john-hay bib check.

--- checker.py
+++ checker.py
@@ -23,17 +23,21 @@ log = logging.getLogger(__name__)
 def run_page_checks():
     """ Manages functional-checks for bib-pages. """
     try:
-        page_checks.check_A()         # `David Beckwith papers`
-        yoken = YokenCheck()          # `Mel B. Yoken collection`
-        yoken.run_check()
-        john_hay = JohnHayCheck()     # `John Hay papers`
-        john_hay.run_check()
-        gregorian = GregorianCheck()  # `Vartan Gregorian papers`
-        gregorian.run_check()
-        brown = BrownCheck()          # `John Nicholas Brown II papers`
-        brown.run_check()
-        microfilm = MiscMicrofilmCheck()
-        microfilm.run_check()
+        # page_checks.check_A()         # `David Beckwith papers`
+        # yoken = YokenCheck()          # `Mel B. Yoken collection`
+        # yoken.run_check()
+        # john_hay = JohnHayCheck()     # `John Hay papers`
+        # john_hay.run_check()
+        # gregorian = GregorianCheck()  # `Vartan Gregorian papers`
+        # gregorian.run_check()
+        # brown = BrownCheck()          # `John Nicholas Brown II papers`
+        # brown.run_check()
+        # microfilm = MiscMicrofilmCheck()
+        # microfilm.run_check()
+
+        h_jh = MiscHayJohnHayCheck()
+        h_jh.run_check()
+
     except Exception:
         log.exception( 'exception; traceback...' )
         # raise
@@ -41,16 +45,16 @@ def run_page_checks():
 def run_results_checks():
     """ Manages functional-checks for bib-pages. """
     try:
-        beckwith = BeckwithResultsCheck()    # `David Beckwith papers`
-        beckwith.run_check()
-        yoken = YokenResultsCheck()          # `Mel B. Yoken collection`
-        yoken.run_check()
+        # beckwith = BeckwithResultsCheck()    # `David Beckwith papers`
+        # beckwith.run_check()
+        # yoken = YokenResultsCheck()          # `Mel B. Yoken collection`
+        # yoken.run_check()
         john_hay = JohnHayResultsCheck()     # `John Hay papers`
         john_hay.run_check()
-        gregorian = GregorianResultsCheck()  # `Vartan Gregorian papers`
-        gregorian.run_check()
-        brown = BrownResultsCheck()          # `John Nicholas Brown II papers`
-        brown.run_check()
+        # gregorian = GregorianResultsCheck()  # `Vartan Gregorian papers`
+        # gregorian.run_check()
+        # brown = BrownResultsCheck()          # `John Nicholas Brown II papers`
+        # brown.run_check()
     except Exception:
         log.exception( 'exception; traceback...' )
         # raise
